d969e9b57ac9147f955843a06ec5210f80016ae2
joaovpr <joao.romao@ccc.ufcg.edu.br>
2019-10-01 12:08:57 -0300

    :)

--- lista3/P11 - Pentagon/p11.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-import math
-num = int(raw_input())
-for i in range(num):
-    lado = int(raw_input())
-    area_pen = (5*(lado**2))/(4*(math.sqrt(5-2*(math.sqrt(5)))))
-    print "%.3f" % (area_pen)
